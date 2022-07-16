249341e253ff8f4d896aa6ac43d112e5b7054a27
dylan <dylanzam@gmail.com>
2022-07-16 11:52:35 +0200

    check duplicates before storing. store users,reacts, post_fixed,post_engagment

--- fb_scraper/__init__.py
+++ fb_scraper/__init__.py
@@ -18,9 +18,9 @@ reg_identifier = r'^(?:.*)\/(?:pages\/[A-Za-z0-9-]+\/)?(?:profile\.php\?id=)?([A
 def rsleep(t, cap=10, q=True):
     sleep = t+np.random.randint(0, cap)
     if not q: 
-        print(f'Sleep for {sleep} seconds...', end='')
+        print(f'Sleep for {sleep} seconds...', end='', flush=True)
     time.sleep(sleep)
-    if not q: print('done')
+    if not q: print('done', flush=True)
 
 id2react = {
     1: 'LIKE',
@@ -69,6 +69,8 @@ class FacebookStorer:
                 print(f'User {link} identifer not recognized')
                 continue
 
+            print(f'storing {identifier}')
+
             user_exists = False
             if not identifier.isdecimal():
                 print(identifier, react_type)
@@ -108,11 +110,81 @@ class FacebookStorer:
     def store_comment(self, comment_id, user_id, text):
         raise NotImplementedError
 
-    def store_post_fixed(self):
-        raise NotImplementedError
+    def store_post_fixed(self, info):
+        exists_query = """
+            SELECT COUNT(*) FROM post_fixed WHERE post_id='{}'
+        """
+        insert_query = """
+            INSERT INTO post_fixed VALUE (%s, %s, %s, %s, %s, %s)
+        """
 
-    def store_post_engagements(self):
-        raise NotImplementedError
+        rows = []
+        if isinstance(info, dict):
+            info = [info]
+        elif not isinstance(info, list):
+            print('must pass list of dicts or single dict')
+
+        for post in info:
+            post_id = post['post_id']
+            currquery = exists_query.format(post_id)
+            exists = self.conn.execute(currquery).iloc[0][0]
+            if exists: 
+                print(f'Post {post_id} already exists')
+                continue
+            row = (
+                post_id,
+                post['page_id'],
+                post['has_text'],
+                post['has_video'],
+                post['has_image'],
+                post['post_time'],
+            )
+            rows.append(row)
+
+        self.conn.insert(insert_query, rows)
+        return rows
+
+    def store_post_engagements(self, info):
+        exists_query = """
+            SELECT COUNT(*) FROM post_engagement WHERE post_id='{}' AND scrape_date='{}'
+        """
+        insert_query = """
+            INSERT INTO post_engagement VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        rows = []
+        if isinstance(info, dict):
+            info = [info]
+        elif not isinstance(info, list):
+            print('must pass list of dicts or single dict')
+        
+        scrape_date= datetime.now().date().strftime('%Y-%m-%d')
+
+        for post in info:
+            post_id = post['post_id']
+            currquery = exists_query.format(post_id, scrape_date)
+            exists = self.conn.execute(currquery).iloc[0][0]
+            if exists: 
+                print(f'Post {post_id} on {str(scrape_date)} already exists')
+                continue
+
+            row = (
+                post_id,
+                scrape_date,
+                post['num_shares'],
+                post['num_comments'],
+                post['num_reacts'],
+                post['num_like'],
+                post['num_haha'],
+                post['num_love'],
+                post['num_wow'],
+                post['num_sad'],
+                post['num_angry'],
+            )
+            rows.append(row)
+
+        self.conn.insert(insert_query, rows)
+        return rows
 
     def store_react(self, reacts: list = None, user_id: str = None, post_id: int = None, react_type: int = None, force: bool = False):
         scrape_date = datetime.now().date()
@@ -120,6 +192,7 @@ class FacebookStorer:
             reacts = [(user_id, post_id, react_type)]
 
         rows = []
+        print('storing reacts...')
         for user_id, post_id, react_type in reacts:
             react_exists_query = f"""
                 SELECT COUNT(*) 
@@ -146,21 +219,36 @@ class FacebookStorer:
             insert_query = 'INSERT INTO post_reacts VALUES (%s, %s, %s, %s)'
             row = (scrape_date, post_id, user_id, react_id)
             rows.append(row)
-        print(rows)
         self.conn.insert(insert_query, rows)
         return rows
 
-    def store_simple_user(self, users = None, user_id = None, name = None, surname = None, force=False):
-
-        if users is None:
-            users = [(user_id, name, surname)]
+    def store_simple_user(self, info):
+        exists_query = """
+            SELECT COUNT(*) FROM users_simple WHERE user_id='{}'
+        """
+        
+        insert_query = """
+            INSERT INTO users_simple VALUE (%s, %s, %s, %s)
+        """
 
         rows = []
-        for user_id, name, surname in users:
-            user_exists_query = f"SELECT COUNT(*) FROM users_simple WHERE user_id='{user_id}'"
-            exists = self.conn.execute(user_exists_query).iloc[0][0]
-            if exists and not force: return
-            print(f'storing user id = {user_id}')
+        if isinstance(info, dict):
+            info = [info]
+        elif not isinstance(info, list):
+            print('must pass list of dicts or single dict')
+        # memory inefficient for large db 
+        exists = self.conn.execute('SELECT user_id FROM users_simple').values
+
+        for user in info:
+            user_id = user['user_id']
+            print(f'storing {user_id}')
+            name = user['name']
+            #currquery = exists_query.format(user_id)
+            #exists = self.conn.execute(currquery).iloc[0][0]
+            if user_id in exists: 
+                print(f'{user_id} already exists')
+                continue
+
             d = getgender.Detector(case_sensitive=False)
             gender = d.get_gender(name)
             if 'female' in gender:
@@ -169,16 +257,18 @@ class FacebookStorer:
                 gender = 'M'
             else:
                 gender = None
-            rows.append((user_id, name, surname, gender))
 
-        insert_query = """
-            INSERT INTO users_simple VALUES
-            (%s, %s, %s, %s)
-        """
-        print(rows)
+            row = (
+                user_id,
+                name,
+                user['surname'],
+                gender
+            )
+            rows.append(row)
+
+        rows = list(set(rows))
         self.conn.insert(insert_query, rows)
         return rows
-
 
     # TODO: store user string identifier. Allows for easy lookup
     def store_full_user(self, user_id: int, info=None, force=False):
@@ -247,7 +337,7 @@ class FacebookStorer:
             'comments': True, 
             'sharers': True, 
             'reactors': True,
-            'progress': True,
+            #'progress': True,
             'posts_per_page': 20
         }
 
@@ -256,84 +346,115 @@ class FacebookStorer:
         params_eng = []
         post_reacts = []
         new_users = []
+        posts_fixed = []
         for i, post in enumerate(posts):
-            if i >= num_posts: break
-            post_id = post['post_id']
-            print(post_id)
-            post_time = post['time']
-            content = post['text']
-            num_comments = post['comments']
-            num_shares = post['shares']
-            has_image = post['image'] is not None
-            has_video = post['video'] is not None
-            video_watches = post['video_watches']
-            was_live = post['was_live']
+            try:
+                if i >= num_posts: break
+                post_id = post['post_id']
+                print(post_id)
+                post_time = post['time']
+                has_text = True if post['text'] is not None else False
+                #content = post['text']
+                num_comments = post['comments']
+                num_shares = post['shares']
+                has_image = post['image'] is not None
+                has_video = post['video'] is not None
+                video_watches = post['video_watches']
+                was_live = post['was_live']
 
-            for sharer in post['sharers']:
-                name_surname = sharer['name']
-                name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
-                link = sharer['link']
-                sharer_id = re.match(reg_identifier, link).group(1)
+                posts_fixed.append({
+                    'post_id': post_id,
+                    'page_id': page_id,
+                    'has_text': has_text,
+                    'has_video': has_video,
+                    'has_image': has_image,
+                    'post_time': post_time
+                })
 
-            reactions = post['reactions']
-            if reactions is None: reactions = {}
-            num_like = reactions.get('like', 0)
-            num_love = reactions.get('love', 0)
-            num_haha = reactions.get('haha', 0)
-            num_wow = reactions.get('wow', 0)
-            num_angry = reactions.get('angry', 0)
-            num_sad = reactions.get('sad', 0)
-            num_reacts = post.get('reaction_count', 0)
+                for sharer in post['sharers']:
+                    name_surname = sharer['name']
+                    name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
+                    link = sharer['link']
+                    sharer_id = re.match(reg_identifier, link).group(1)
 
-            params_eng.append((
-                post_id,
-                scrape_date,
-                num_shares,
-                num_comments,
-                num_reacts,
-                num_like,
-                num_haha,
-                num_love,
-                num_wow,
-                num_sad,
-                num_angry,
-            ))
+                reactions = post['reactions']
+                if reactions is None: reactions = {}
+                num_like = reactions.get('like', 0)
+                num_love = reactions.get('love', 0)
+                num_haha = reactions.get('haha', 0)
+                num_wow = reactions.get('wow', 0)
+                num_angry = reactions.get('angry', 0)
+                num_sad = reactions.get('sad', 0)
+                num_reacts = post.get('reaction_count', 0)
 
-            reactors = post['reactors']
-            if reactors == None: reactors = []
-            for reactor in reactors:
-                name_surname = reactor['name']
-                name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
-                react_str = reactor['type'].upper()
-                react_type = react2id.get(react_str)
-                if react_type is None:
-                    print(f'Special react {react_str} for {name_surname}')
-                    continue
-                link = reactor['link']
-                reactor_id = re.match(reg_identifier, link).group(1)
-                post_reacts.append((reactor_id, post_id, react_type))
-                new_users.append((reactor_id, name, surname))
+                params_eng.append({
+                    'post_id': post_id,
+                    'scrape_date': scrape_date,
+                    'num_shares': num_shares,
+                    'num_comments': num_comments,
+                    'num_reacts': num_reacts,
+                    'num_like': num_like,
+                    'num_haha': num_haha,
+                    'num_love': num_love,
+                    'num_wow': num_wow,
+                    'num_sad': num_sad,
+                    'num_angry': num_angry,
+                })
+
+                reactors = post['reactors']
+                if reactors == None: reactors = []
+                for reactor in reactors:
+                    name_surname = reactor['name']
+                    name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
+                    react_str = reactor.get('type')
+                    if react_str is None:
+                        print(f'None type react for {name_surname}')
+                        continue
+
+                    react_str = react_str.upper()
+                    react_type = react2id.get(react_str)
+                    if react_type is None:
+                        print(f'Special react {react_str} for {name_surname}')
+                        continue
+                    link = reactor['link']
+                    reactor_id = re.match(reg_identifier, link).group(1)
+                    post_reacts.append((reactor_id, post_id, react_type))
+                    new_users.append({
+                        'user_id': reactor_id, 
+                        'name': name, 
+                        'surname': surname
+                    })
 
 
-            for comment in post['comments_full']:
-                comment_id = comment['comment_id']
-                name_surname = comment.get('commenter_name')
-                name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
-                link = comment['commenter_url']
-                comment_time = comment['comment_time']
-                commentor_id = re.match(reg_identifier, link).group(1)
-                comment_reactions = comment.get('comment_reaction_count', 0)
-                text = comment['comment_text']
-                #ALSO STORE REPLIES
-                new_users.append((commentor_id, name, surname))
+                for comment in post['comments_full']:
+                    comment_id = comment['comment_id']
+                    name_surname = comment.get('commenter_name')
+                    name, surname = name_surname.split()[0], ' '.join(name_surname.split()[1:])
+                    link = comment['commenter_url']
+                    comment_time = comment['comment_time']
+                    commentor_id = re.match(reg_identifier, link).group(1)
+                    comment_reactions = comment.get('comment_reaction_count', 0)
+                    #text = comment['comment_text']
+                    #ALSO STORE REPLIES
+                    new_users.append({
+                        'user_id': commentor_id,
+                        'name': name,
+                        'surname': surname
+                    })
+            except TemporarilyBanned as e:
+                print(f'Temporarily banned')
+                break
 
             rsleep(10, 10, q=False)
 
-        insert_eng = """
-            INSERT INTO post_engagement VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-
         print('storing data...')
+        if len(posts_fixed):
+            rows = self.store_post_fixed(posts_fixed)
+            print(f'{len(rows)} rows of new posts inserted')
+        else:
+            print('No new posts to insert')
+
+
         if len(new_users):
             rows = self.store_simple_user(new_users)
             print(f'{len(rows)} rows of new users inserted')
@@ -342,13 +463,12 @@ class FacebookStorer:
 
         if len(post_reacts):
             rows = self.store_react(post_reacts)
-            print(f'{len(params_eng)} rows of engagement data inserted')
+            print(f'{len(params_eng)} rows of reacts inserted')
         else:
             print('No post reacts to insert')
 
-        # TODO: check for duplicates first!!
         if len(params_eng):
-            self.conn.insert(insert_eng, params_eng)
+            self.store_post_engagements(params_eng)
             print(f'{len(params_eng)} rows of engagement data inserted')
         else:
             print('No engagameent data to insert')
@@ -362,7 +482,7 @@ if __name__ == '__main__':
     fb = FacebookStorer(cookies)
 
     print('storing posts')
-    fb.store_posts(page_name, 10)
+    fb.store_posts(page_name, 3)
     breakpoint()
 
     from fb_api import MyGraphAPI
