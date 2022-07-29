3769736f3fbd44a35b6fdaa0820697ce2584d470
dylan <dylanzam@gmail.com>
2022-07-29 17:03:36 +0200

    store aggregated stuff for owned pages

--- insight/fb_api/update.py
+++ insight/fb_api/update.py
@@ -14,6 +14,7 @@ class PageExtractor:
         self.api = MyGraphAPI(page=page)
         self.page = Page(page)
         self.storage = Storage()
+        self.react_types = ['LIKE', 'LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY', 'NONE']
 
     def store(self):
         self.storage.store(self.page)
@@ -23,18 +24,66 @@ class PageExtractor:
     
     def pages(self, is_competitor):
         self.page.is_competitor = is_competitor
+        return self
 
     def page_engagement(self):
         fields = 'id,fan_count'
         res = self.api.get_object(self.page.page_id, fields=fields)
 
         self.page.num_likes = res['fan_count']
+        return self
 
     def post_comments(self):
         pass
 
     def post_fixed(self):
-        pass
+        api_posts = self.api.get_object(self.page.page_id, fields='posts')['posts']['data']
+        for api_post in api_posts:
+            page_post_id = api_post['id']
+            print(f'reading post {page_post_id}')
+
+            react_field = ','.join(['reactions.type({}).limit(0).summary(1).as({})'.format(react, react) for react in self.react_types])
+            fields = 'attachments,reactions.limit(0).summary(1).as(TOT),comments.summary(1),shares,created_time,message,permalink_url,' + react_field
+            data = self.api.get_object(page_post_id, fields=fields)
+            
+            attachments = data['attachments']['data']
+            media = next((att['media'] for att in attachments if 'media' in att), {})
+            has_image = 'image' in media
+            has_video = 'video' in media
+            num_shares = data.get('shares', {'count': 0})['count']
+            num_comments = data['comments']['summary']['total_count']
+            create_time = data['created_time']
+            num_reacts = data['TOT']['summary']['total_count']
+            num_like = data['LIKE']['summary']['total_count']
+            num_love = data['LOVE']['summary']['total_count']
+            num_wow = data['WOW']['summary']['total_count']
+            num_haha = data['HAHA']['summary']['total_count']
+            num_angry = data['ANGRY']['summary']['total_count']
+            num_sad = data['SAD']['summary']['total_count']
+            message = data.get('message', '')
+            has_text = message != ''
+
+            post = Post(
+                num_shares=num_shares,
+                num_comments=num_comments,
+                num_reacts=num_reacts,
+                num_like=num_like,
+                num_love=num_love,
+                num_wow=num_wow,
+                num_haha=num_haha,
+                num_angry=num_angry,
+                num_sad=num_sad,
+                has_video=has_video,
+                has_image=has_image,
+                has_text=has_text,
+                was_live=None,
+                post_id=page_post_id,
+                post_time=create_time,
+                page_id=self.page.page_id
+            )
+            self.page.posts.append(post)
+
+        return self
 
     def post_reacts(self):
         pass
@@ -42,53 +91,12 @@ class PageExtractor:
     def post_shares(self):
         pass
 
-    def react_types(self):
-        pass
-
     def users(self):
         pass
 
-competitors = []
-
-def main(pages):
-    """
-    only specify pages we have page_access_token for now
-    """
-
-    conn = MySQLConnector(os.environ.get('SOCIAL_CONN'))
-
-    insert_query = 'INSERT INTO page VALUES (%s, %s, %s, %s, %s, %s, %s)';
-
-    if not isinstance(pages, list):
-        pages = [pages]
-
-    params = []
-    df = {}
-    for page in pages:
-        api = MyGraphAPI(page=page)
-
-        fields = 'id,fan_count,link,overall_star_rating,rating_count,name'
-        res = api.get_object(page_name, fields=fields)
-        today = pd.Timestamp.now().date()
-        res['scrape_date'] = str(today)
-        num_photos = len(api.get_object(f'{page_name}/photos')['data'])
-        num_posts = len(api.get_object(f'{page_name}/posts')['data'])
-
-        res['num_photos'] = num_photos
-        res['num_posts'] = num_posts
-
-        params.append((
-            res['id'],
-            res['scrape_date'],
-            res['fan_count'],
-            res['overall_star_rating'],
-            res['rating_count'],
-            res['num_posts'],
-            res['num_photos']
-        ))
-    print(params)
-    conn.insert(insert_query, params)
 
 if __name__ == '__main__':
     page_name = 'levelupmalta'
-    main(page_name)
+    extractor = PageExtractor(page_name)
+    extractor = extractor.pages(0).page_engagement().post_fixed()
+    extractor.store()
