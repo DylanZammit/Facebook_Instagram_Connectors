dd86b3fb4187cd30e8f7b81b41f4d0a32ec08df5
dylan <dylanzam@gmail.com>
2022-09-04 19:01:25 +0200

    bump version 0.0.8 -> 0.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Distutils',
-      version='0.0.8',
+      version='0.0.9',
       description='Python Distribution Utilities',
       author='dylan zammit',
       author_email='dylanzam@gmail.com',
