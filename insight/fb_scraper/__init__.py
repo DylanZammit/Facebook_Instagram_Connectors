8786bb21c53394f81b1869033590e47b715aed43
dylan <dylanzam@gmail.com>
2022-09-11 10:40:47 +0200

    argparse literal %

--- insight/fb_scraper/__init__.py
+++ insight/fb_scraper/__init__.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MCMC setup args.')
     parser.add_argument('--page_name', help='page name or id [default=timesofmalta]', type=str, default='timesofmalta')
     parser.add_argument('--n_posts', help='Number of posts to read [default=inf]', type=int)
-    parser.add_argument('--posts_since', help='datetime to read posts since: "%Y-%m-%d %H:%M:%S', type=str)
+    parser.add_argument('--posts_since', help='datetime to read posts since: "%%Y-%%m-%%d %%H:%%M:%%S', type=str)
     parser.add_argument('--posts_lookback', help='num of hours since now', type=int)
     parser.add_argument('--commenters', help='read and store commentors', action='store_true') 
     parser.add_argument('--sharers', help='read and store sharers', action='store_true') 
