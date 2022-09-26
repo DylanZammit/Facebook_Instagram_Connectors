33f18adde075eb1d1be5f1d2cec471361e6b861f
dylan <dylanzam@gmail.com>
2022-09-26 21:56:34 +0200

    bump version 0.0.19 -> 0.0.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='insight',
-      version='0.0.19',
+      version='0.0.20',
       description='insight tools',
       author='dylan zammit',
       author_email='dylanzam@gmail.com',
