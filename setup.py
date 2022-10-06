dc37dfb33f2ea771870c0466e1e01795cd68ac8f
EC2 Default User <ec2-user@ip-172-31-16-94.eu-west-1.compute.internal>
2022-10-06 07:41:00 +0000

    bump version 0.0.29 -> 0.0.30

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='insight',
-      version='0.0.29',
+      version='0.0.30',
       description='insight tools',
       author='dylan zammit',
       author_email='dylanzam@gmail.com',
