512fed14388961dde29c2a1b5ab85ded2124eec2
dylan <dylanzam@gmail.com>
2022-10-08 20:34:41 +0200

    bump version 0.0.31 -> 0.0.32

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='insight',
-      version='0.0.31',
+      version='0.0.32',
       description='insight tools',
       author='dylan zammit',
       author_email='dylanzam@gmail.com',
