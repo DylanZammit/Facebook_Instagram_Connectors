f22b06bf57102652943476d41d11f3aeae6b70dc
EC2 Default User <ec2-user@ip-172-31-16-94.eu-west-1.compute.internal>
2022-10-10 05:21:16 +0000

    cast media pk to int

--- insight/instagram/scraper/update.py
+++ insight/instagram/scraper/update.py
@@ -96,6 +96,7 @@ class InstaScraper(Client):
             media_ptype = media.product_type
             media_type = get_media_content(media_type, media_ptype.upper())
             media.media_type = media_type
+            media.pk = int(media.pk)
 
         return medias
 
