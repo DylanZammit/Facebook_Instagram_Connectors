ef99722156921d741564c74b22ea7a5f0fa5e949
EC2 Default User <ec2-user@ip-172-31-16-94.eu-west-1.compute.internal>
2022-10-27 19:57:44 +0000

    refresh pbi script: print->logger, run once mode

--- scheduled_jobs/refresh_powerbi.py
+++ scheduled_jobs/refresh_powerbi.py
@@ -1,10 +1,15 @@
 from powerbi.client import PowerBiClient
 from powerbi.session import PowerBiSession
+import os
 import time
 import pandas as pd
+from logger import mylogger, pb
 from traceback import format_exc
 from datetime import datetime, timedelta
 
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+logger = mylogger('refresh_powerbi/refresh')
 
 class Refresh:
     def __init__(
@@ -18,9 +23,11 @@ class Refresh:
         ):
         self.datasets = [] if datasets is None else datasets
 
-
+        credentials = os.path.join(BASE_PATH, credentials)
         self.group = group
         self.dataflow = dataflow
+
+        logger.info(credentials)
 
         # Initialize the Client.
         power_bi_client = PowerBiClient(
@@ -37,16 +44,16 @@ class Refresh:
 
     def dataflow_status(self):
         transaction_state = self.pbs.make_request('get',f'myorg/groups/{self.group}/dataflows/{self.dataflow}/transactions?$top=1')
-        #print(transaction_state)
+        logger.info(transaction_state)
         status = transaction_state['value'][0]['status']
-        print(f'Dataflow status: {status}')
+        logger.info(f'Dataflow status: {status}')
         return status
 
     def dataset_status(self, dataset):
         transaction_state = self.pbs.make_request('get',f'myorg/groups/{self.group}/datasets/{dataset}/refreshes?$top=1')
         status = transaction_state['value']
         status = status[0]['status'] if len(status) == 9 else 'boq'
-        print(f'Dataset status: {status}')
+        logger.info(f'Dataset status: {status}')
         return status
 
     def wait_dataflow_refresh(self):
@@ -56,7 +63,7 @@ class Refresh:
         if self.dataflow_status() == 'Failed':
             raise ValueError(f'dataflow={self.dataflow} refresh failed')
 
-    def refresh(self, delay:int=120, until:str='2030-01-01'):
+    def refresh(self, delay:int=120, until:str='2030-01-01', once:bool=False):
         until = pd.Timestamp(until)
         while datetime.now() < until:
             try:
@@ -76,28 +83,28 @@ class Refresh:
                             dataset_refreshed[dataset] = res
                             time.sleep(0.1)
                     time.sleep(10)
-
             except Exception as e:
-                print('error:' + format_exc())
-            print(f'{datetime.now()}: Sleeping {delay}s before next refresh')
+                logger.critical('error:' + format_exc())
+            logger.info(f'{datetime.now()}: Sleeping {delay}s before next refresh')
+            if once: break
             time.sleep(delay)
     
     def refresh_dataflow(self):
         try:
             self.pbs.make_request('post',f'myorg/groups/{self.group}/dataflows/{self.dataflow}/refreshes')
-            print('dataflow Refresh started')
+            logger.info('dataflow Refresh started')
             return 1
         except Exception as e:
-            print('error:' + format_exc())
+            logger.critical('error:' + format_exc())
             return 0
 
     def refresh_dataset(self, dataset):
         try:
             self.pbs.make_request('post', f'myorg/groups/{self.group}/datasets/{dataset}/refreshes')
-            print(f'Refresh dataset={dataset} request sent!')
+            logger.info(f'Refresh dataset={dataset} request sent!')
             return 1
         except Exception as e:
-            print('error:' + format_exc())
+            logger.critical('error:' + format_exc())
             return 0
 
 
@@ -109,12 +116,13 @@ if __name__ == '__main__':
     parser.add_argument('--name', help='name of group of dataflows/datasets you want to run as specified in yaml', type=str, required=True)
     parser.add_argument('--delay', help='delay between next refresh in seconds', type=int, default=60)
     parser.add_argument('--until', help='when should refresh stop', type=str, default='2030-01-01')
+    parser.add_argument('--once', help='refresh once', action='store_true')
     args = parser.parse_args()
 
-    with open(args.config) as f:
+    with open(os.path.join(BASE_PATH, args.config)) as f:
         kwargs = yaml.safe_load(f)
         kwargs.update(kwargs.pop(args.name))
 
-    Refresh(**kwargs).refresh(delay=args.delay, until=args.until)
+    Refresh(**kwargs).refresh(delay=args.delay, until=args.until, once=args.once)
 
 
